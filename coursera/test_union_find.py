ce8cd3e82a5c4f296bab3f9f2aca1954951f22bc
unknown <dieterdervermieter@gmail.com>
2013-02-14 14:31:57 +0100

    refactoring union_find

---
+++ coursera/test_union_find.py
@@ -0,0 +1,13 @@
+from union_find import UnionFind
+
+def test_connected():
+	union = UnionFind(3)
+	assert union.connected(0,1) == False
+	assert union.connected(1,2) == False
+	union.union(0,1)
+	assert union.connected(0,1)
+	assert union.connected(1,2) == False
+	union.union(1,2)
+	assert union.connected(2,0)
+	assert union.connected(0,2)
+	assert union.connected(1,2)
