5508c8ae43d0fc2a1d88fe6433454b88f752fe62
unknown <dieterdervermieter@gmail.com>
2013-02-18 21:56:53 +0100

    made it a little less ugly

--- coursera/union_weighted_union.py
+++ coursera/union_weighted_union.py
@@ -6,13 +6,14 @@ class UnionFind(object):
 		p_root = self.root(p)
 		q_root = self.root(q)
 		if(self._size_[p_root] < self._size_[q_root]):
-			self._nodes_[p_root] = q_root
-			self._size_[q_root] += self._size_[p_root]
+			self.subordinate(p_root, q_root)
 		else:
-			self._nodes_[self.root(q)] = self.root(p)
-			self._size_[p_root] += self._size_[q_root]
+			self.subordinate(q_root, p_root)
 	def connected(self,p,q):
 		return self.root(p) == self.root(q)
+	def subordinate(self, smallRoot, bigRoot):
+		self._nodes_[smallRoot] = bigRoot
+		self._size_[bigRoot] += self._size_[smallRoot]
 	def root(self, x):
 		if(x != self._nodes_[x]):
 			return self.root(self._nodes_[x])
