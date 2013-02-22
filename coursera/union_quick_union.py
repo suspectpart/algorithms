439ac73ce79ad75b4fa8f2d9adf955fc0527ef26
Johannes Hofmeister <cessor@github.com>
2013-02-22 22:29:55 +0100

    Included a hint for you! You don't have to iterate the range to use it as a list directly

--- coursera/union_quick_union.py
+++ coursera/union_quick_union.py
@@ -1,6 +1,6 @@
 class UnionFind(object):
 	def __init__(self,n):
-		self.nodes = [x for x in range(0,n)]
+		self.nodes = range(n)
 	def union(self, p, q):
 		self.nodes[self.root(p)] = self.root(q)
 	def connected(self,p,q):
