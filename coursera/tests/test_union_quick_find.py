e786e65b075fd48558209df9bbb65dfd3add5dbb
Horst Schneider <dieterdervermieter@gmail.com>
2013-02-24 16:22:49 +0100

    deleted example tests. thanks, johannes!

--- coursera/tests/test_union_quick_find.py
+++ coursera/tests/test_union_quick_find.py
@@ -1,8 +1,9 @@
+from nose.tools import *
 from union_quick_find import UnionFind
 
 def test_connected():
 	union = UnionFind(3)
-	assert union.connected(0,1) == False
+	assert_equal(union.connected(0,1), False)
 	assert union.connected(1,2) == False
 	union.union(0,1)
 	assert union.connected(0,1)
