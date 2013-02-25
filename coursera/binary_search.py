2b19ed9b14bc1575814a2a761ef75e8b7c1ae539
Horst Schneider <dieterdervermieter@gmail.com>
2013-02-25 13:07:12 +0100

    changed the binary search to use recursion

--- coursera/binary_search.py
+++ coursera/binary_search.py
@@ -2,14 +2,13 @@ class BinarySearch(object):
 	def __init__(self, numbers):
 		self.numbers = numbers
 	def search(self, number):
-		lo, hi = 0, len(self.numbers) - 1
-		
-		while lo <= hi:
-			mid = lo + (hi-lo) / 2
-			if number < self.numbers[mid]:
-				hi = mid - 1
-			elif number > self.numbers[mid]:
-				lo = mid + 1
-			else:
-				return mid
-		return -1
+		return self._search(number, lo = 0, hi = len(self.numbers) -1)
+	def _search(self, number, lo, hi):
+		if lo > hi:
+			return -1
+		mid = lo + (hi-lo) / 2
+		if number < self.numbers[mid]:
+			return self._search(number, lo, hi = mid - 1)
+		if number > self.numbers[mid]:
+			return self._search(number, lo = mid + 1, hi)
+		return mid
