fdb34fd0f50150614c87f889be5a15e75991dc9e
Horst Schneider <dieterdervermieter@gmail.com>
2013-02-27 17:31:30 +0100

    added a note to self

--- coursera/selection_sort.py
+++ coursera/selection_sort.py
@@ -1,6 +1,10 @@
 class SelectionSort():
 	'''
-	implemented in sedgewick style!
+	implemented sedgewick style!
+	
+	As we always have to loop because we can't know where the minimum item is,
+	this algorithm will always perform ~ N^2 / 2 compares, no matter if the input is already sorted or not.
+	That sucks because - as sedgewick told us - quadratic time is no good.
 	'''
 	def sort(self, items):
 		for i, current in enumerate(items):
