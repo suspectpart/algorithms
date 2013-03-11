d3a9deee8719327ddd6564c68b42c8fb9a07b42b
Horst Schneider <dieterdervermieter@gmail.com>
2013-03-11 13:57:55 +0100

    added vector stuff

---
+++ vectors.py
@@ -0,0 +1,32 @@
+import math
+
+class Vector(object):
+	def __init__(self, a, b, c):
+		self.coords = (a,b,c)
+
+	def scalar_product(self, other):
+		return sum([x * y for x, y in zip(self.coords, other.coords)])
+
+	def orthogonal_to(self, other):
+		return self.scalar_product(other) == 0
+
+	def add(self, other):
+		a1, b1, c1 = self.coords
+		a2, b2, c2 = other.coords
+		return Vector(a1 + a2, b1 + b2, c1 + c2)
+
+	def subtract(self, other):
+		a1, b1, c1 = self.coords
+		a2, b2, c2 = other.coords
+		return Vector(a1 - a2, b1 - b2, c1 - c2)
+
+	def magnitude(self):
+		return math.sqrt(sum([x**2 for x in self.coords]))
+
+	def angle(self, other):
+		return math.degrees(math.acos(self.scalar_product(other) / (self.magnitude() * other.magnitude())))
+
+	def parallel_to(self, other):
+		a1, b1, c1 = self.coords
+		a2, b2, c2 = other.coords
+		return a2 / a1 == b2 / b1 == c2 / c1
